b2e93ef64067f26f4fec598700247ec9ad5e7d39
lakaens <lakaens12@gmail.com>
2017-06-01 13:28:34 +0200

    last update gamepad

--- PROJECT GUNSMOKE MARC/ModuleInput.h
+++ PROJECT GUNSMOKE MARC/ModuleInput.h
@@ -5,8 +5,11 @@
 #include "Globals.h"
 #include "SDL\include\SDL_scancode.h"
 #include"SDL\include\SDL.h"
+#include"p2Point.h"
 
 #define MAX_KEYS 300
+
+#define JOYSTICK_DEAD_ZONE 8000
 
 enum KEY_STATE
 {
@@ -14,6 +17,33 @@ enum KEY_STATE
 	KEY_DOWN,
 	KEY_REPEAT,
 	KEY_UP
+};
+
+enum GAMEPAD_STATE
+{
+	PAD_BUTTON_IDLE = 0,
+	PAD_BUTTON_DOWN,
+	PAD_BUTTON_REPEAT,
+	PAD_BUTTON_KEY_UP
+};
+
+struct Gamepad {
+	GAMEPAD_STATE A = PAD_BUTTON_IDLE;
+	GAMEPAD_STATE B;
+	GAMEPAD_STATE Y;
+	GAMEPAD_STATE X;
+	GAMEPAD_STATE START;
+	GAMEPAD_STATE BACK;
+	GAMEPAD_STATE CROSS_UP;
+	GAMEPAD_STATE CROSS_DOWN;
+	GAMEPAD_STATE CROSS_LEFT;
+	GAMEPAD_STATE CROSS_RIGHT;
+	p2Point<float> movementJoystick;
+	bool joystickUp;
+	bool joystickDown;
+	bool joystickLeft;
+	bool joystickRight;
+
 };
 
 class ModuleInput : public Module
@@ -29,6 +59,11 @@ public:
 
 public:
 	KEY_STATE keyboard[MAX_KEYS];
+	Gamepad gamepad;
+	void GamepadButtons();
+
+private:
+	SDL_Event event;
 	SDL_GameController* gcontroller;
 };
 
